6d1f0ec5d1799db0bc6d2b1496f27c7234c51cbd
nilina <natalialychkovskaya@pb-d-128-141-229-241.cern.ch>
2014-07-01 11:09:24 +0200

    phi_symmetry

---
+++ HcalProm/src/phiSym.cc
@@ -0,0 +1,353 @@
+// *********************** for reco data **********************
+// system include files
+#include <memory>
+/*
+#include <stdio.h>
+#include <math.h>
+#include <sstream>
+#include <fstream>
+#include <exception>
+#include <string>
+*/
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/Selector.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+
+#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+#include "Geometry/CaloGeometry/interface/CaloSubdetectorGeometry.h"
+#include "Geometry/CaloGeometry/interface/CaloCellGeometry.h"
+#include "Geometry/Records/interface/CaloGeometryRecord.h"
+
+#include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
+#include "RecoLocalCalo/HcalRecAlgos/interface/HcalCaloFlagLabels.h"
+
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
+#include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
+#include "DataFormats/HcalDetId/interface/HcalSubdetector.h"
+#include "DataFormats/Common/interface/TriggerResults.h"
+
+#include "CondFormats/HcalObjects/interface/HcalQIEShape.h"
+#include "CondFormats/HcalObjects/interface/HcalQIECoder.h"
+#include "CalibFormats/HcalObjects/interface/HcalDbService.h"
+#include "CalibFormats/HcalObjects/interface/HcalDbRecord.h"
+#include "CondFormats/HcalObjects/interface/HcalPedestals.h"
+#include "CalibFormats/HcalObjects/interface/HcalCalibrations.h"
+
+//#include "DataFormats/METReco/interface/HcalNoiseSummary.h"
+//#include "CommonTools/RecoAlgos/interface/HBHENoiseFilter.h"
+
+//TFile Service
+
+#include "FWCore/Common/interface/TriggerNames.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+//
+#include "TLorentzVector.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TProfile.h"
+#include <TROOT.h>
+#include <TSystem.h>
+#include "TFile.h"
+#include <TCanvas.h>
+#include <cmath>
+#include "TMath.h"
+
+using namespace cms;
+using namespace edm;
+using namespace std;
+using namespace reco;
+
+//
+// class declaration
+//
+
+class phiSym : public edm::EDAnalyzer {
+ public:
+  explicit phiSym (const edm::ParameterSet&);
+  ~phiSym();
+
+ private:
+  //virtual void beginJob(const edm::EventSetup&);
+  virtual void beginJob();
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  virtual void endJob() ;
+
+  edm::Service<TFileService> fs;
+
+  std::string histfile;
+  std::string textfile;
+  std::string gainfile;
+  // InputTag triglabel;
+
+  TFile* mFile;
+  FILE* tFile;
+
+  Int_t runNumb, EventN, NvtxEv;
+
+  TH1F *hcounter,*herun, *hBX, *hvertex;
+  TH1F *hen[26][36][2];
+  TH1F *henhbp[16][72][2], *henhbm[16][72][2];
+  TH1F *henhep[14][72][3], *henhem[14][72][3];
+
+  InputTag HBHENoiseFilterResultLabel_;
+  HcalCalibrations calibs_;
+};
+
+// constructors and destructor
+//
+phiSym::phiSym(const edm::ParameterSet& iConfig)
+{
+  //histfile = iConfig.getUntrackedParameter<string>("rootFile");
+  textfile = iConfig.getUntrackedParameter<string>("textFile");
+  gainfile = iConfig.getUntrackedParameter<string>("gainFile");
+  //  triglabel   = iConfig.getUntrackedParameter<InputTag>("triggerResultsLabel");
+  //HBHENoiseFilterResultLabel_ = iConfig.getParameter<edm::InputTag>("HBHENoiseFilterResultLabel");
+}
+
+phiSym::~phiSym()
+{
+  //delete mFile;
+}
+
+// member functions
+
+// ------------ method called to for each event  ------------
+void phiSym::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+
+   //printf("Starting :\n");
+   edm::EventID eventId = iEvent.id();
+   int runNumber = eventId.run ();
+   int eventNumber = eventId.event ();
+   runNumb=runNumber;
+   EventN++;
+
+   edm::Handle<reco::VertexCollection> pvHandle;
+   iEvent.getByLabel("offlinePrimaryVertices",pvHandle);
+   const reco::VertexCollection & vertices = *pvHandle.product();
+  
+   NvtxEv = 0;
+
+   reco::Vertex::Point vert;
+
+   for(reco::VertexCollection::const_iterator it = vertices.begin() ; it != vertices.end() ; ++it)  {
+     if(!(*it).isFake() && (*it).ndof() > 4  ) {
+	 NvtxEv++;
+     }}
+
+
+   //   if (NvtxEv<16)  return;
+
+   //triggers
+   
+   edm::Handle<edm::TriggerResults> triggerResults;
+   edm::InputTag trigResultsTag("TriggerResults","","HLT");
+   iEvent.getByLabel(trigResultsTag,triggerResults);
+   const edm::TriggerNames & triggerNames = iEvent.triggerNames(*triggerResults);
+
+   //std::cout<<" Analysis "<<triggerNames.size()<<std::endl;                                                                                                                     
+
+   for(unsigned i=0; i<triggerNames.size(); i++) {
+
+     cout<<" Trigger name "<<triggerNames.triggerName(i)<<std::endl;
+
+      if(triggerResults->accept(i)) {
+        cout<<"1    "<<(int)(triggerNames.triggerName(i)).find("DoubleMu7")<<endl;
+	cout<<"2    "<<(int)(triggerNames.triggerName(i)).find("Mu13_Mu8")<<endl;
+        cout<<"3    "<<(int)(triggerNames.triggerName(i)).find("Mu17_Mu8")<<endl ;
+        cout<<"4    "<<(int)(triggerNames.triggerName(i)).find("Mu17_TkMu8")<<endl;
+        cout<<"5    "<<(int)(triggerNames.triggerName(i)).find("Mu22_TkMu8")<<endl ;
+        cout<<"6    "<<(int)(triggerNames.triggerName(i)).find("Mu22_TkMu22")<<endl ;
+        cout<<"7    "<<	(int)(triggerNames.triggerName(i)).find("HLT_IsoMu24_eta2p1")<<endl  ;
+        cout<<"8    "<<	(int)(triggerNames.triggerName(i)).find("HLT_IsoMu24")<<endl  ;
+	//	if((int)(triggerNames.triggerName(i)).find("HLT_IsoMu24_eta2p1") == 0 ) { cout <<"trigger failed********************"<<endl;
+	//                                                                        return;
+	//	}
+
+      }
+   }
+   hcounter->Fill(0); // total
+
+   Int_t nBX=0, iBX=1, nORBIT=0;
+   nBX = iEvent.bunchCrossing();
+   //nORBIT = iEvent.orbitNumber();
+
+   //printf("run= %d   ev= %d :\n",runNumber,eventNumber);
+
+   hcounter->Fill(1);
+
+   /*
+   // ------------- HBHE cleaning -----------------------------
+
+   Handle<bool> theHcalFilterResult;
+   iEvent.getByLabel(InputTag("HBHENoiseFilterResultProducer:HBHENoiseFilterResult"), theHcalFilterResult);
+   if (*theHcalFilterResult) {}
+   else {
+     hcounter->Fill(12);
+     return;
+   }
+   */
+   /*
+   edm::Handle<bool> HBHENoiseFilterResultHandle;
+   iEvent.getByLabel(HBHENoiseFilterResultLabel_, HBHENoiseFilterResultHandle);
+   bool HBHENoiseFilterResult = *HBHENoiseFilterResultHandle;
+   if (!HBHENoiseFilterResultHandle.isValid()) {
+     std::cout<<"Could not find HBHENoiseFilterResult"<<std::endl;
+   }
+   bool bHcalNoiseFilter = HBHENoiseFilterResult;
+   if(!bHcalNoiseFilter) {
+     hcounter->Fill(10);
+     return;
+   }
+   */
+   //hf rechits
+   Handle<HFRecHitCollection> hf_hits_h;
+   iEvent.getByLabel("hfreco", hf_hits_h);
+   const HFRecHitCollection* hf_hits = hf_hits_h.failedToGet () ? 0 : &*hf_hits_h;
+
+   //hcal rechits
+   Handle<HBHERecHitCollection> hbhe_hits_h;
+   iEvent.getByLabel("hbhereco", hbhe_hits_h);
+   const HBHERecHitCollection* hbhe_hits = hbhe_hits_h.failedToGet () ? 0 : &*hbhe_hits_h;
+
+   double Etot=0;
+   int jeta,jphi,jdepth;
+
+   // ------------ HF -----------
+   if (hf_hits_h.isValid()) {   
+
+     for (HFRecHitCollection::const_iterator hfhit=hf_hits->begin(); hfhit!=hf_hits->end(); hfhit++) {
+       HcalDetId detId = hfhit->id();
+       int ieta = hfhit->id().ieta();
+       int iphi = hfhit->id().iphi();
+       int depth = hfhit->id().depth();
+       double energy = hfhit->energy();
+       double trec = hfhit->time();
+       if (ieta>0) jeta = ieta-29;
+       else jeta = 13-ieta-29;
+       jphi=iphi/2;
+       jdepth=depth-1;
+       //int flB = hfhit->flagField(HcalCaloFlagLabels::HFDigiTime); // status flag
+       //int flSTD = hfhit->flagField(HcalCaloFlagLabels::HFLongShort);
+       //if (flB+flSTD<=0) 
+       hen[jeta][(iphi-1)/2][depth-1]->Fill(energy);
+       if (energy>10) Etot += energy;
+     }
+   }
+   else printf("No HF RecHits: run= %d  ev= %d :\n",runNumber,eventNumber); // ------------ 
+
+   // ------------ HBHE -----------
+   if (hbhe_hits_h.isValid()) {   
+     hcounter->Fill(3);
+
+     for (HBHERecHitCollection::const_iterator hbhehit=hbhe_hits->begin(); hbhehit!=hbhe_hits->end(); hbhehit++) {
+       int ieta = hbhehit->id().ieta();
+       int iphi = hbhehit->id().iphi();
+       int depth = hbhehit->id().depth();
+       double energy = hbhehit->energy();
+       double trec = hbhehit->time();
+       
+       //printf("%3d %3d %3d %f\n",ieta,iphi,depth,energy);
+       
+       if (abs(ieta)>16 || (abs(ieta)==16 && depth==3)) { // HE
+	 if (ieta>0) henhep[ieta-16][iphi-1][depth-1]->Fill(energy);
+	 else  henhem[-ieta-16][iphi-1][depth-1]->Fill(energy);
+	 if (energy>4) Etot += energy;
+       }
+       else { // HB
+	 if (ieta>0) henhbp[ieta-1][iphi-1][depth-1]->Fill(energy);
+	 else henhbm[-ieta-1][iphi-1][depth-1]->Fill(energy);
+	 if (energy>4) Etot += energy;
+       }
+     }
+   }
+   else printf("No RecHits: run= %d  ev= %d :\n",runNumber,eventNumber); // ------------ 
+
+   herun->Fill(runNumber,Etot); 
+   hBX->Fill(nBX,Etot); 
+   hvertex->Fill(NvtxEv);
+
+  return;
+}
+
+void phiSym::beginJob() {
+
+  EventN=0;
+
+  char htit[64];
+  if ((tFile = fopen(textfile.c_str(),"w"))==NULL) printf("\nNo textfile open\n\n");
+
+  herun = fs->make< TH1F>("herun","E(HF) vs Nrun;Nrun;GeV",11000,189000,200000);
+  hBX = fs->make< TH1F>("hBX","E(HF) vs nBX;BX;GeV",4096,0,4096);
+  hcounter = fs->make< TH1F>("hcounter","hcounter",101,-0.5,100.5);
+  hvertex = fs->make< TH1F>("hvertex","hvertex",100, 0, 99.5);
+  /// hvertex = fs->AddVariable( "NvtxEv", &NvtxEv );
+  TFileDirectory ESpec = fs->mkdir( "espec" );
+  for (int i=0;i<13;i++) for (int j=0;j<36;j++) for (int k=0;k<2;k++) {
+    if (i>10 && j%2==0) continue;
+    sprintf(htit,"E_+%d_%d_%d",i+29,j*2+1,k+1);
+    hen[i][j][k] = ESpec.make< TH1F>(htit,htit,1000,0,250); // E Rec
+    sprintf(htit,"E_-%d_%d_%d",i+29,j*2+1,k+1);
+    hen[i+13][j][k] = ESpec.make< TH1F>(htit,htit,1000,0,250);
+  }
+
+  TFileDirectory EBSpec = fs->mkdir( "eHBspec" );
+  for (int i=0;i<16;i++) for (int j=0;j<72;j++) for (int k=0;k<2;k++) {
+    if (i+1<15 && k==1) continue;
+    sprintf(htit,"E_+%d_%d_%d",i+1,j+1,k+1);
+    henhbp[i][j][k] = EBSpec.make< TH1F>(htit,htit,1000,0,250); // E Rec
+    sprintf(htit,"E_-%d_%d_%d",i+1,j+1,k+1);
+    henhbm[i][j][k] = EBSpec.make< TH1F>(htit,htit,1000,0,250);
+  }
+
+  TFileDirectory EESpec = fs->mkdir( "eHEspec" );
+  for (int i=0;i<14;i++) for (int j=0;j<72;j++) for (int k=0;k<3;k++) {
+    if (i+16==16 && k<2) continue;
+    if (i+16==17 && k>0) continue;
+    if (i+16>17 && i+16<27 && k==2) continue;
+    if (i+16==29 && k==2) continue;
+    if (i+16>20 && (j+1)%2==0) continue;
+    sprintf(htit,"E_+%d_%d_%d",i+16,j+1,k+1);
+    henhep[i][j][k] = EESpec.make< TH1F>(htit,htit,1000,0,250); // E Rec
+    sprintf(htit,"E_-%d_%d_%d",i+16,j+1,k+1);
+    henhem[i][j][k] = EESpec.make< TH1F>(htit,htit,1000,0,250);
+  }
+
+  std::cout<<std::endl<<"beginJob: histfile="<<histfile.c_str()<<"  textfile="<<textfile.c_str()<<std::endl;
+  return;
+
+}
+
+void phiSym::endJob() {
+
+  fprintf(tFile,"#RunN %d   Events processed %d\n",runNumb,EventN);
+  std::cout<<"endJob: histos processing..."<<std::endl;
+  std::cout<<"RunN= "<<runNumb<<"  Events processed= "<<EventN<<std::endl;
+
+  fclose(tFile);
+  //fclose(gFile);
+  std::cout<<std::endl<<" --endJob-- done"<<std::endl;
+  return;
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(phiSym);
---
+++ HcalProm/test/CRAB/d524phiSymRAW.py
@@ -0,0 +1,97 @@
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("PHASEHFX")
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000
+process.MessageLogger.cerr.default.limit = 10
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load("Configuration.StandardSequences.GeometryDB_cff")
+process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+
+#--- Global Tag conditions
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+from Configuration.AlCa.autoCond import autoCond
+#process.GlobalTag.globaltag = autoCond['mc'] #mc
+process.GlobalTag.globaltag = autoCond['startup'] #data
+#process.GlobalTag.globaltag = 'GR_E_V25::All'
+#process.GlobalTag.globaltag = 'GR_R_52_V7::All'
+
+process.es_pool = cms.ESSource("PoolDBESSource",  
+                                process.CondDBSetup,
+                                timetype = cms.string('runnumber'),
+                                toGet = cms.VPSet(
+     cms.PSet(
+              record = cms.string("HcalRespCorrsRcd"),
+              tag = cms.string("HcalRespCorrs_v4.5_offline")),
+      cms.PSet(
+             record = cms.string('HcalGainsRcd'),
+             tag = cms.string('HcalGains_v5.07_offline')),
+
+     ),
+     connect = cms.string('frontier://FrontierProd/CMS_COND_31X_HCAL'),
+     authenticationMethod = cms.untracked.uint32(0),
+)
+process.es_prefer_es_pool = cms.ESPrefer( "PoolDBESSource", "es_pool")
+
+# summary
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False),
+					SkipEvent = cms.untracked.vstring('ProductNotFound')
+)
+
+# BPTX & BSC triggers filter
+process.load('L1TriggerConfig.L1GtConfigProducers.L1GtTriggerMaskTechTrigConfig_cff')
+process.load("HLTrigger.HLTfilters.hltLevel1GTSeed_cfi")
+process.hltLevel1GTSeed.L1TechTriggerSeeding = cms.bool(True)
+process.hltLevel1GTSeed.L1SeedsLogicalExpression = cms.string('NOT (36 OR 37 OR 38 OR 39) AND NOT ((42 AND NOT 43) OR (43 AND NOT 42))')
+
+process.phaseHF = cms.EDAnalyzer ("phiSymRAW",
+    textFile = cms.untracked.string('yhisto_6.txt'),
+    gainFile = cms.untracked.string('gain_6.txt'),
+    triggerResultsLabel = cms.InputTag("TriggerResults::HLT"),
+    l1GtUnpack = cms.untracked.string('l1GtUnpack')
+)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('yhisto_6.root'),
+)
+
+# -------------------------  HBHEHF RECO
+
+from EventFilter.HcalRawToDigi.HcalRawToDigi_cfi import *
+
+from RecoLocalCalo.HcalRecProducers.HcalHitReconstructor_hbhe_cfi import *
+#from RecoLocalCalo.HcalRecProducers.HcalHitReconstructor_ho_cfi import *
+from RecoLocalCalo.HcalRecProducers.HcalHitReconstructor_hf_cfi import *
+#from RecoLocalCalo.HcalRecProducers.HBHEIsolatedNoiseReflagger_cfi import *
+process.newhbheprereco = hbheprereco.clone()
+#process.newhbhereco    = hbhereco.clone()
+#process.newhoreco      = horeco.clone()
+process.newhfreco      = hfreco.clone()
+#process.newhbhereco.hbheInput    = "newhbheprereco"
+process.newhcalLocalRecoSequence = cms.Sequence(process.newhbheprereco+process.newhfreco)
+#process.newhcalLocalRecoSequence = cms.Sequence(process.newhbheprereco+process.newhbhereco+process.newhfreco+process.newhoreco)
+
+#--------------------------- Making re-digi/re-reco and analysing
+#process.hcalDigis.InputLabel = 'source' # data RAW input
+
+process.p = cms.Path(
+ process.hcalDigis *
+ process.newhcalLocalRecoSequence *
+ process.phaseHF
+) 
+
+process.source = cms.Source ("PoolSource" ,
+fileNames=cms.untracked.vstring(
+'/store/data/Run2012B/DoubleElectron/RAW/v1/000/195/930/7A3B6D92-87B2-E111-9F8D-001D09F291D7.root',
+'/store/data/Run2012B/DoubleElectron/RAW/v1/000/195/930/6C248C45-7BB2-E111-B34B-BCAEC518FF8E.root',
+'/store/data/Run2012B/DoubleElectron/RAW/v1/000/195/930/5AD5A8F2-94B2-E111-AE1E-5404A63886C1.root',
+'/store/data/Run2012B/DoubleElectron/RAW/v1/000/195/930/52670532-83B2-E111-95AE-BCAEC5364C4C.root'
+)
+)
+import FWCore.PythonUtilities.LumiList as LumiList
+#process.source.lumisToProcess = LumiList.LumiList(filename = 'Cert_190456-208686_8TeV_PromptReco_Collisions12_JSON.tx').getVLuminosityBlockRange()
+ 
